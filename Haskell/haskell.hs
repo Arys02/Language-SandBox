436c9a46ff4224daff25871aca64c55593d93fbc
Charles Yaiche <yaiche_c@epita.fr>
2016-02-23 12:08:38 +0100

    small test haskell and lisp

--- Haskell/haskell.hs
+++ Haskell/haskell.hs
@@ -3,5 +3,14 @@ ssq 1 = 1
 ssq n = n * n + ssq (n - 1)
 
 hypo :: Float -> Float -> Float
-hype a b = sqrt ((a * a) + (b * b))
+hypo a b = sqrt ((a * a) + (b * b))
 
+backwards :: [a] -> [a]
+backwards = reverse
+
+-- mapping 
+map sqrt [1..5]
+
+-- folding
+foldr1 (+) [1..5]
+sum [1..5]
--- Lisp/test.lisp
+++ Lisp/test.lisp
@@ -5,5 +5,14 @@
   1
   (+ (* n n) (ssq (i - n)))))
 
+
 (defun hypo (a b)
   (sqrt (+ (* a a) (* b b))))
+
+(setf (symbol-function 'backwards) #'reverse)
+
+;; mapping
+(mapcar #'sqrt '(1 2 3 4 5))
+
+;; folding
+(reduce #'+ '(1 2 3 4 5))
