f3afa67ca8aa14b82898061ac00e43aabdd90884
Charles Yaiche <yaiche_c@epita.fr>
2016-03-08 11:06:28 +0100

    lisp/haskell some add

--- Haskell/haskell.hs
+++ Haskell/haskell.hs
@@ -14,3 +14,19 @@ map sqrt [1..5]
 -- folding
 foldr1 (+) [1..5]
 sum [1..5]
+
+-- anonyme function
+(\x -> 2 * x) 4
+
+adder :: int -> (int -> int)
+adder n = \x -> x + n
+
+ssq :: int -> int
+ssq 1 = 1
+ssq n = n * n + ssq(n + 1)
+
+sqrtp :: float -> float -> bool
+sqrtp s x = s > 0 && s * s == x
+
+intlist :: int -> [int]
+intlist s = s : intlist (s + 1)
--- Lisp/test.lisp
+++ Lisp/test.lisp
@@ -16,3 +16,18 @@
 
 ;; folding
 (reduce #'+ '(1 2 3 4 5))
+
+(defun say-hello (who)
+(format t "adding something")
+(format t "Hello ~A!~%" who)
+
+;; anomyme function
+(lambda (x) (* 2 x) 3)
+
+(defun adder (n)
+  lambda (x) (+ x n)))
+
+(defun sqrtp (s x)
+  and (> s 0)
+  = (* s s) x)
+
